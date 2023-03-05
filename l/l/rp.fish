6e5a4cf04a31191d9bffd4b27203d2633966858b
liqowomo <123171512+liqowomo@users.noreply.github.com>
2023-03-05 15:38:27 +0000

    🛰️

---
+++ l/l/rp.fish
@@ -0,0 +1,34 @@
+#!/bin/fish 
+
+#*******************************************DRINKHERPISS***********************************************************************
+
+### SCANS API ### 
+
+set LETH Q1D5Z7Q7B5EP84IGT69EA3E92STYGP45RR
+set LMUM HEGDN2R3N7684K6M9B983ZQMGAMWMQHEZ3
+set LSEP CWPJSZX9EA1ZDTATM4D5PJXJ8U8T2KR57C 
+
+### ALCHEMY RPCS ### 
+
+set LAETH https://eth-mainnet.g.alchemy.com/v2/nab-2AFbLteBD46nMZeHKW--lkmQmlNa
+set LAGOE https://eth-goerli.g.alchemy.com/v2/wfowE284fYsqh-laeF5qmjOTQ3YTwuX_
+set LAMUM https://polygon-mumbai.g.alchemy.com/v2/DlxcFxU5u-OioYGMvKhwWsAzAknhvQYd
+set LAARG https://arb-goerli.g.alchemy.com/v2/-6a6qvz5RGQhqJD5q9CUw25LRoNa-N5t
+
+### MORALIS ### 
+set LMORA yL8KDGXJrrRXwRrgZMAEUvCUM13sCXmNuBwUYvAmMl2BVER5kNn8zz3CQmJv3b1A
+
+### INFURA RPC ### 
+set LIETH https://mainnet.infura.io/v3/4d9f7fa54ce44d1aa3319dca50aa3dd7
+set LIGOE https://goerli.infura.io/v3/4d9f7fa54ce44d1aa3319dca50aa3dd7
+set LISEP https://sepolia.infura.io/v3/4d9f7fa54ce44d1aa3319dca50aa3dd7
+set LIFIL https://2L00pf1WwNdm3vcCFp7oH74MK48:8a72b1099ec87787c160d5a488c6b972@filecoin.infura.io
+
+### QuickNode BSC ### 
+set LQBSCT https://sparkling-boldest-brook.bsc-testnet.discover.quiknode.pro/9c0a1ccf523e238a5e6d574a36ea192f5fcfb747
+
+### PUBIC RPC ### 
+# ANKR PUBLIC RPC - https://www.ankr.com/rpc/
+# CHAINLIST RPC - https://chainlist.org/
+
+#*******************************************SMELLHERFART***********************************************************************  
