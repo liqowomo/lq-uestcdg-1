aedc7709d6fbecfb0653d318f20e6d9d5432031b
liqowomo <123171512+liqowomo@users.noreply.github.com>
2023-03-05 17:03:28 +0000

    🛰️

--- l/l/st.sh
+++ l/l/st.sh
@@ -49,6 +49,7 @@ sudo apt install cmake -y
 sudo apt install neofetch -y
 cargo install-update -a
 cargo install cargo-update
+cargo install cargo-modules
 rustup update
 sudo apt-add-repository ppa:fish-shell/release-3 -y 
 sudo apt install libwebkit2gtk-4.0-dev libgtk-3-dev libappindicator3-dev -y
