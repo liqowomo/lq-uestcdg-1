1aa561267a2936f40eeaf825fa03ad7153d9f32e
liqowomo <123171512+liqowomo@users.noreply.github.com>
2023-03-06 16:26:46 +0000

    🛰️

--- l/l/rp.fish
+++ l/l/rp.fish
@@ -32,3 +32,19 @@ set LQBSCT https://sparkling-boldest-brook.bsc-testnet.discover.quiknode.pro/9c0
 # CHAINLIST RPC - https://chainlist.org/
 
 #*******************************************SMELLHERFART***********************************************************************  
+
+# Set Bastard Walley from l/l/4.txt
+
+set LW1 0x498b302db295199b81af90Df66F330D5dA2776D0
+set LW1K 0x608a510934421f52fa4636080a8765f742122c527aec807fb83900ae4ed57a64
+
+
+set LW2 0xfB4E8AfEaB22c5a7AC8F4c719D2D75b93bdc5CFa
+set LW2K 0xbfd81cadf97bed4c4a5f75b12f1b5e9315eee787f24a9b14b1c94dde2f139bba
+
+set LW3 0x9436E627D55c5c7d8247bEa3d5E35B9ab8e44006
+set LW3K 0x49643a1fac5e213c767dad6f7654d5288b564b5bda1c39c2c340e184649f0a71
+
+set LW4 0xFB4086b4Ae138B2e91B64f47385ffdac8112f526
+set LW4K 0x8e6362e5d9a4a281680161a50c8fb5898929095b18ebbd58c716d844814cce24
+
