e007198a49b961304d7d8562af90143437bfc9f0
liqowomo <123171512+liqowomo@users.noreply.github.com>
2023-03-06 18:55:40 +0000

    🛰️

---
+++ l/l/w4.py
@@ -0,0 +1,164 @@
+#!/usr/bin/env python3
+
+'''
+Extract all tx for an account using moralis
+https://docs.moralis.io/web3-data-api/evm/how-to-get-all-transactions-of-an-address
+- Pythonic method is the easiet
+- This one is going to be your own
+'''
+
+from pprint import pprint
+from moralis import evm_api
+import requests
+
+api_key = "yL8KDGXJrrRXwRrgZMAEUvCUM13sCXmNuBwUYvAmMl2BVER5kNn8zz3CQmJv3b1A"
+
+# Colors classes
+
+
+class colors:
+
+    '''
+    Colors class:reset all colors with colors.reset; two
+    sub classes fg for foreground
+    and bg for background; use as colors.subclass.colorname.
+    i.e. colors.fg.red or colors.bg.greenalso, the generic bold, disable,
+    underline, reverse, strike through,
+    and invisible work with the main class i.e. colors.bold
+    '''
+    reset = '\033[0m'
+    bold = '\033[01m'
+    disable = '\033[02m'
+    underline = '\033[04m'
+    reverse = '\033[07m'
+    strikethrough = '\033[09m'
+    invisible = '\033[08m'
+
+    class fg:
+        black = '\033[30m'
+        red = '\033[31m'
+        green = '\033[32m'
+        orange = '\033[33m'
+        blue = '\033[34m'
+        purple = '\033[35m'
+        cyan = '\033[36m'
+        lightgrey = '\033[37m'
+        darkgrey = '\033[90m'
+        lightred = '\033[91m'
+        lightgreen = '\033[92m'
+        yellow = '\033[93m'
+        lightblue = '\033[94m'
+        pink = '\033[95m'
+        lightcyan = '\033[96m'
+
+    class bg:
+        black = '\033[40m'
+        red = '\033[41m'
+        green = '\033[42m'
+        orange = '\033[43m'
+        blue = '\033[44m'
+        purple = '\033[45m'
+        cyan = '\033[46m'
+        lightgrey = '\033[47m'
+
+### Function 1 ###
+### Print transactions in from sepolia network ###
+
+
+def t1():
+    params1 = {
+        "address": "0xFB4086b4Ae138B2e91B64f47385ffdac8112f526",
+        "chain": "sepolia",
+    }
+
+    result1 = evm_api.transaction.get_wallet_transactions(
+        api_key=api_key,
+        params=params1,
+    )
+    print(colors.fg.blue,
+          '''
+✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧
+✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧
+✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧
+          '''
+          )
+    print(colors.fg.blue,
+          '''
+███████╗ ███████╗ ██████╗   ██████╗  ██╗      ██╗  █████╗ 
+██╔════╝ ██╔════╝ ██╔══██╗ ██╔═══██╗ ██║      ██║ ██╔══██╗
+███████╗ █████╗   ██████╔╝ ██║   ██║ ██║      ██║ ███████║
+╚════██║ ██╔══╝   ██╔═══╝  ██║   ██║ ██║      ██║ ██╔══██║
+███████║ ███████╗ ██║      ╚██████╔╝ ███████╗ ██║ ██║  ██║
+╚══════╝ ╚══════╝ ╚═╝       ╚═════╝  ╚══════╝ ╚═╝ ╚═╝  ╚═╝
+       ''')
+    pprint(result1)
+
+#######
+# Function 2 find out internal transactions for mymbai
+#
+
+
+def t2():
+    params2 = {
+        "address": "0xFB4086b4Ae138B2e91B64f47385ffdac8112f526",
+        "chain": "mumbai",
+    }
+
+    result2 = evm_api.transaction.get_wallet_transactions(
+        api_key=api_key,
+        params=params2,
+    )
+    print(colors.fg.purple,
+          '''
+✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧
+✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧
+✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧
+         '''
+          )
+    print(colors.fg.purple,
+          '''
+███╗   ███╗ ██╗   ██╗ ███╗   ███╗ ██████╗   █████╗  ██╗
+████╗ ████║ ██║   ██║ ████╗ ████║ ██╔══██╗ ██╔══██╗ ██║
+██╔████╔██║ ██║   ██║ ██╔████╔██║ ██████╔╝ ███████║ ██║
+██║╚██╔╝██║ ██║   ██║ ██║╚██╔╝██║ ██╔══██╗ ██╔══██║ ██║
+██║ ╚═╝ ██║ ╚██████╔╝ ██║ ╚═╝ ██║ ██████╔╝ ██║  ██║ ██║
+╚═╝     ╚═╝  ╚═════╝  ╚═╝     ╚═╝ ╚═════╝  ╚═╝  ╚═╝ ╚═╝
+          ''')
+    pprint(result2)
+
+# Function 2 find out internal transactions on bsc
+
+
+def t3():
+    params3 = {
+        "address": "0xFB4086b4Ae138B2e91B64f47385ffdac8112f526",
+        "chain": "bsc testnet",
+    }
+
+    result3 = evm_api.transaction.get_wallet_transactions(
+        api_key=api_key,
+        params=params3,
+    )
+    print(colors.fg.orange,
+          '''
+✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧
+✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧
+✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧
+         '''
+          )
+    print(colors.fg.orange,
+          '''
+██████╗  ███████╗  ██████╗
+██╔══██╗ ██╔════╝ ██╔════╝
+██████╔╝ ███████╗ ██║     
+██╔══██╗ ╚════██║ ██║     
+██████╔╝ ███████║ ╚██████╗
+╚═════╝  ╚══════╝  ╚═════╝
+          ''')
+    pprint(result3)
+
+
+# Call Defined functions from above
+t1()
+t2()
+t3()
